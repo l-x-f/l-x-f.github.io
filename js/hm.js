7e583410acbfa84f65594d680a3eb6a0b1de1bd4
xiaofei <lixiaofei0627@gmail.com>
2024-10-16 14:25:33 +0800

    deploy

---
+++ js/hm.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || []
+;(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?c44260bf55ab13d28a551ed3697559e1'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
+
